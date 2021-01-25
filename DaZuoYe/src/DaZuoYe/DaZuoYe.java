6ae1b9d4688d5c91ba9890e6e0acc490833f772e
STFive <77976140+STFive@users.noreply.github.com>
2021-01-25 14:54:00 +0000

    Add files via upload

---
+++ DaZuoYe/src/DaZuoYe/DaZuoYe.java
@@ -0,0 +1,14 @@
+package DaZuoYe;
+
+
+import java.io.File;
+
+public class DaZuoYe {
+    public static void main(String[] args){
+        File json = new File("src/resources/2803301701.json");
+
+        System.out.println();
+    }
+
+
+}
